aa02ad0589ed8b007b477ec72c5e2fcdc9c4d4e3
FerencHegedus <hegedus.ferenc.82@gmail.com>
2020-05-27 20:54:54 +0200

    Tutorial T8 (renamed from T7)

--- Tutorials_SingleSystem_PerThread/T8_MultiGPUSingleNode/MultiGPUSingleNode_SystemDefinition.cuh
+++ Tutorials_SingleSystem_PerThread/T8_MultiGPUSingleNode/MultiGPUSingleNode_SystemDefinition.cuh
@@ -1,44 +1,76 @@
-#ifndef MULTIGPUSINGLENODE_SYSTEMDEFINITION_H
-#define MULTIGPUSINGLENODE_SYSTEMDEFINITION_H
+#ifndef T8_MULTIGPUSINGLENODE_SYSTEMDEFINITION_H
+#define T8_MULTIGPUSINGLENODE_SYSTEMDEFINITION_H
 
 // SYSTEM
-__device__ void PerThread_OdeFunction(int tid, int NT, double* F, double* X, double T, double* cPAR, double* sPAR, int* sPARi, double* ACC, int* ACCi)
+template <class Precision>
+__forceinline__ __device__ void PerThread_OdeFunction(\
+			int tid, int NT, \
+			Precision*    F, Precision*    X, Precision     T, \
+			Precision* cPAR, Precision* sPAR, int*      sPARi, Precision* ACC, int* ACCi)
 {
 	F[0] = X[1];
 	F[1] = X[0] - X[0]*X[0]*X[0] - cPAR[0]*X[1] + sPAR[0]*cos(T);
 }
 
 // EVENTS
-__device__ void PerThread_EventFunction(int tid, int NT, double* EF, double* X, double T, double* cPAR, double* sPAR, int* sPARi, double* ACC, int* ACCi)
+template <class Precision>
+__forceinline__ __device__ void PerThread_EventFunction(\
+			int tid, int NT, Precision* EF, \
+			Precision     T, Precision    dT, Precision*    TD, Precision*   X, \
+			Precision* cPAR, Precision* sPAR,       int* sPARi, Precision* ACC, int* ACCi)
 {
 	EF[0] = X[1];
 	EF[1] = X[0];
 }
 
-__device__ void PerThread_ActionAfterEventDetection(int tid, int NT, int IDX, int CNT, double &T, double &dT, double* TD, double* X, double* cPAR, double* sPAR, int* sPARi, double* ACC, int* ACCi)
+template <class Precision>
+__forceinline__ __device__ void PerThread_ActionAfterEventDetection(\
+			int tid, int NT, int IDX, int& UDT, \
+			Precision    &T, Precision   &dT, Precision*    TD, Precision*   X, \
+			Precision* cPAR, Precision* sPAR, int*       sPARi, Precision* ACC, int* ACCi)
 {
 	if ( X[0] > ACC[0] )
 		ACC[0] = X[0];
 	
-	if ( (IDX==1) && (CNT==2) )
+	if ( IDX == 1 )
+		ACCi[0]++;
+	
+	if ( (IDX ==1 ) && ( ACCi[0] == 2 ) )
 		ACC[1] = X[1];
 }
 
 // ACCESSORIES
-__device__ void PerThread_ActionAfterSuccessfulTimeStep(int tid, int NT, double T, double dT, double* TD, double* X, double* cPAR, double* sPAR, int* sPARi, double* ACC, int* ACCi)
+template <class Precision>
+__forceinline__ __device__ void PerThread_ActionAfterSuccessfulTimeStep(\
+			int tid, int NT, int& UDT, \
+			Precision&    T, Precision&   dT, Precision*    TD, Precision*   X, \
+			Precision* cPAR, Precision* sPAR, int*       sPARi, Precision* ACC, int* ACCi)
 {
 	if ( X[0] > ACC[2] )
 		ACC[2] = X[0];
 }
 
-__device__ void PerThread_Initialization(int tid, int NT, double T, double &dT, double* TD, double* X, double* cPAR, double* sPAR, int* sPARi, double* ACC, int* ACCi)
+template <class Precision>
+__forceinline__ __device__ void PerThread_Initialization(\
+			int tid, int NT, int& DOIDX, \
+			Precision&    T, Precision&   dT, Precision*    TD, Precision*   X, \
+			Precision* cPAR, Precision* sPAR,       int* sPARi, Precision* ACC, int* ACCi)
 {
+	T      = TD[0]; // Reset the starting point of the simulation from the lower limit of the time domain
+	DOIDX  = 0;     // Reset the start of the filling of dense output from the beggining
+	
 	ACC[0] = X[0];
 	ACC[1] = X[1];
 	ACC[2] = X[0];
+	
+	ACCi[0] = 0; // Event counter of the second event function
 }
 
-__device__ void PerThread_Finalization(int tid, int NT, double T, double dT, double* TD, double* X, double* cPAR, double* sPAR, int* sPARi, double* ACC, int* ACCi)
+template <class Precision>
+__forceinline__ __device__ void PerThread_Finalization(\
+			int tid, int NT, int& DOIDX, \
+			Precision&    T, Precision&   dT, Precision*    TD, Precision*   X, \
+			Precision* cPAR, Precision* sPAR,       int* sPARi, Precision* ACC, int* ACCi)
 {
 	
 }
