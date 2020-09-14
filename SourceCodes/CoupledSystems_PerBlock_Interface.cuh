0bd0c879b5721c1a35b45c315d644f5e91db2166
FerencHegedus <hegedus.ferenc.82@gmail.com>
2020-09-14 13:32:12 +0200

    Fixed, Issue 7, Module Per-Thread, Issue 11, Module Per-Block

--- SourceCodes/CoupledSystems_PerBlock_Interface.cuh
+++ SourceCodes/CoupledSystems_PerBlock_Interface.cuh
@@ -7,6 +7,7 @@
 #include <iomanip>
 #include <fstream>
 #include <string>
+#include <typeinfo>
 #include <cuda_runtime.h>
 
 #define gpuErrCHK(call)                                                                          \
